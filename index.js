a59a1dbb0978237f5a65b9308e5aa90069d2ff72
Umid Kayumov <you@example.com>
2022-10-27 03:54:13 -0700

    changes env

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const cors = require("cors");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config({ path: "./.env" });
-const {DateTime} = require("luxon")
-
-
+const { DateTime } = require("luxon");
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,9 +18,9 @@ app.use(bodyParser.json());
 app.use("/api", router);
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    await mongoose.connect(process.env.MONGO_URL).then(() => {
       console.log("MongoDB connected");
-    })
+    });
     app.listen(PORT, () => {
       console.log(`\nServer started on port ${PORT}🚀`);
       console.log(`http://localhost:${PORT}`);
